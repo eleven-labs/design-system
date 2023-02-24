1a3b526c964fe7f850da28794f11e6a953a2693b
fpasquet <fpasquet@users.noreply.github.com>
2023-02-24 08:51:06 +0000

    deploy: 201bfa7e1fcee2f227428df0bf99b5df03d043a7

---
+++ assets/Box.stories-9cfbfb38.js
@@ -0,0 +1,28 @@
+import{j as s,a as o,F as p}from"./jsx-runtime-670450c2.js";import{B as r,H as x,T as a}from"./Button-62df0b36.js";import{a as g}from"./systemPropsControls-3f7e62c8.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./cssProperties-7e3c2fb7.js";import"./storybookHelper-d524505e.js";const S={title:"Components/Atoms/Layout/Box",component:r,args:{p:"m",bg:"azure",color:"white",children:s(x,{textAlign:"center",children:"I'm a div Box ;"})},parameters:{layout:"centered",controls:{exclude:["as","children"]}},argTypes:{...g}},u=c=>s(r,{...c}),e=u.bind({}),t=()=>o(p,{children:[o(r,{hiddenAbove:"md",children:['This text hides at the "md" value screen width or'," ",s(a,{as:"span",fontWeight:"bold",children:"greater"})]}),o(r,{hiddenBelow:"sm",children:['This text hides at the "sm" value screen width and'," ",s(a,{as:"span",fontWeight:"bold",children:"smaller"})]})]});var d,i,n;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:"args => <Box {...args} />",...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var m,h,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`(): JSX.Element => <>
+    <Box hiddenAbove="md">
+      This text hides at the "md" value screen width or{' '}
+      <Text as="span" fontWeight="bold">
+        greater
+      </Text>
+    </Box>
+    <Box hiddenBelow="sm">
+      This text hides at the "sm" value screen width and{' '}
+      <Text as="span" fontWeight="bold">
+        smaller
+      </Text>
+    </Box>
+  </>`,...(l=(h=t.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};e.parameters={storySource:{source:"args => <Box {...args} />"},...e.parameters};t.parameters={storySource:{source:`(): JSX.Element => <>
+    <Box hiddenAbove="md">
+      This text hides at the "md" value screen width or{' '}
+      <Text as="span" fontWeight="bold">
+        greater
+      </Text>
+    </Box>
+    <Box hiddenBelow="sm">
+      This text hides at the "sm" value screen width and{' '}
+      <Text as="span" fontWeight="bold">
+        smaller
+      </Text>
+    </Box>
+  </>`},...t.parameters};const W=["Overview","HiddenSystemProps"];export{t as HiddenSystemProps,e as Overview,W as __namedExportsOrder,S as default};
+//# sourceMappingURL=Box.stories-9cfbfb38.js.map
--- components/Atoms/Logo/Logo.d.ts
+++ components/Atoms/Logo/Logo.d.ts
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import { ColorSystemProps, LogoNameType, MarginSystemProps } from '../../../types';
-export type LogoProps = Omit<React.SVGProps<SVGSVGElement>, 'color'> & MarginSystemProps & Pick<ColorSystemProps, 'color'> & {
+import { AsProps, ColorSystemProps, LogoNameType, MarginSystemProps } from '../../../types';
+export type LogoProps = AsProps<'div'> & MarginSystemProps & Pick<ColorSystemProps, 'color'> & {
     name: LogoNameType;
     size?: string | number;
 };
-export declare const Logo: React.FC<LogoProps>;
+export declare const Logo: import('../../../types').ComponentWithAs<"div", LogoProps>;
