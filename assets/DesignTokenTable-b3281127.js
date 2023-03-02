1c3170d1e2414790545e527d403597e9caf119dd
fpasquet <fpasquet@users.noreply.github.com>
2023-03-02 18:41:59 +0000

    deploy: 837a4f257eba9d1c77fe8a5269e1913a8cbda5d4

---
+++ assets/DesignTokenTable-b3281127.js
@@ -0,0 +1,2 @@
+import{j as e,F as o}from"./jsx-runtime-670450c2.js";import{T as r}from"./TextHighlight-098d5d24.js";import{T as s}from"./Table-362efa23.js";const T=({title:a,tokenVariables:m})=>e(o,{children:e(s,{title:a,columns:[{name:"name",label:"Name"},{name:"value",label:"Value"}],rows:Object.entries(m).map(([l,n])=>({name:e(r,{fontWeight:"medium",children:l}),value:n.value}))})});export{T as D};
+//# sourceMappingURL=DesignTokenTable-b3281127.js.map
