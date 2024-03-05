98069993361b0f7713a2fa45da09f7377553786e
fpasquet <fpasquet@users.noreply.github.com>
2024-03-05 13:17:15 +0000

    deploy: 4c852710f1f71ca88afb0c32fee22bf98f9ec6cf

---
+++ assets/PostCard.stories-f1da0d7e.js
@@ -0,0 +1,22 @@
+import{j as a}from"./jsx-runtime-ffb262ed.js";import{P as w}from"./Blockquote-478cdd54.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./SyntaxHighlighter-645ec89b.js";import"./extends-98964cd2.js";import"./index-8d47fad6.js";import"./matchMediaQueryList-161f92c8.js";const P={component:w,parameters:{viewport:{defaultViewport:"extraSmallScreen"}}},r={};r.args={contentType:"article",slug:"slug",cover:{img:{src:"https://i.ibb.co/gPtFC17/pexels-matheus-bertelli-1830252.jpg",alt:"cover"}},title:"REX Studio : Intégration de composants React avec Varnish ESI dans un site No Code",date:"09 fév. 2021",readingTime:24,authors:[{username:"jdoe",name:"J. Doe"}],excerpt:"Svelte est en fait un framework et un compilateur, et c’est la principale différence. Svelte est en fait un framework et un compilateur, et c’est la principale différence...",link:{href:"#"}};const o={decorators:[t=>a.jsx("div",{style:{margin:"0 auto",maxWidth:"380px"},children:a.jsx(t,{})})]};o.args={...r.args,variant:"highlight-light"};const e={decorators:[t=>a.jsx("div",{style:{margin:"0 auto",maxWidth:"400px"},children:a.jsx(t,{})})]};e.args={...r.args,variant:"highlight-dark"};const s={decorators:[t=>a.jsx("div",{style:{margin:"0 auto",maxWidth:"764px"},children:a.jsx(t,{})})]};s.args={...r.args,variant:"side-image"};e.parameters={backgrounds:{default:"primary"}};const i={};i.args={...r.args,contentType:"tutorial",tutorialLabel:"Tutoriel"};const n={};n.args={isLoading:!0};var c,m,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(d=(m=r.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var p,g,l;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '380px'
+  }}>
+        <Story />
+      </div>]
+}`,...(l=(g=o.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var u,h,x;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '400px'
+  }}>
+        <Story />
+      </div>]
+}`,...(x=(h=e.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var v,S,y;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '764px'
+  }}>
+        <Story />
+      </div>]
+}`,...(y=(S=s.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};var W,f,j;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:"{}",...(j=(f=i.parameters)==null?void 0:f.docs)==null?void 0:j.source}}};var T,V,k;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:"{}",...(k=(V=n.parameters)==null?void 0:V.docs)==null?void 0:k.source}}};const R=["Overview","WithVariantHighlightLight","WithVariantHighlightDark","WithVariantSideImage","WithContentTypeTutorial","WithIsLoading"];export{r as Overview,i as WithContentTypeTutorial,n as WithIsLoading,e as WithVariantHighlightDark,o as WithVariantHighlightLight,s as WithVariantSideImage,R as __namedExportsOrder,P as default};
