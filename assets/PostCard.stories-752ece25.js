d673e68944e828ea0b7aa0accf9da0e2add0b2df
fpasquet <fpasquet@users.noreply.github.com>
2024-03-08 14:44:10 +0000

    deploy: 0f2e21744bbd83e25a18c107d9bb585d09825b13

---
+++ assets/PostCard.stories-752ece25.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import{P as V}from"./Blockquote-366cca59.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./SyntaxHighlighter-645ec89b.js";import"./extends-98964cd2.js";import"./index-8d47fad6.js";import"./matchMediaQueryList-161f92c8.js";const O={component:V,parameters:{viewport:{defaultViewport:"extraSmallScreen"}}},r={};r.args={contentType:"article",slug:"slug",cover:{img:{src:"https://i.ibb.co/gPtFC17/pexels-matheus-bertelli-1830252.jpg",alt:"cover"}},title:"Phasellus quis mollis ex. Nullam tristique nisl eu orci ullamcorper hendrerit vestibulum elementum metus nulla, scelerisque finibus",date:"09 fév. 2021",readingTime:24,authors:[{username:"jdoe",name:"John Doe"},{username:"jdupont",name:"Jane Dupont"}],excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tempor suscipit. Fusce ac sapien sit amet velit auctor dapibus. Maecenas dui sem, suscipit sagittis leo eget, porta euismod enim.",link:{href:"#"}};const s={decorators:[a=>t.jsx("div",{style:{margin:"0 auto",maxWidth:"380px"},children:t.jsx(a,{})})]};s.args={...r.args,variant:"highlight-light"};const e={decorators:[a=>t.jsx("div",{style:{margin:"0 auto",maxWidth:"400px"},children:t.jsx(a,{})})]};e.args={...r.args,variant:"highlight-dark"};const i={decorators:[a=>t.jsx("div",{style:{margin:"0 auto",maxWidth:"764px"},children:t.jsx(a,{})})]};i.args={...r.args,variant:"side-image"};e.parameters={backgrounds:{default:"primary"}};const o={};o.args={...r.args,contentType:"tutorial",tutorialLabel:"Tutoriel"};const n={};n.args={isLoading:!0};var m,c,u;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(u=(c=r.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var d,p,l;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '380px'
+  }}>
+        <Story />
+      </div>]
+}`,...(l=(p=s.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var g,h,x;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '400px'
+  }}>
+        <Story />
+      </div>]
+}`,...(x=(h=e.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var v,y,S;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    margin: '0 auto',
+    maxWidth: '764px'
+  }}>
+        <Story />
+      </div>]
+}`,...(S=(y=i.parameters)==null?void 0:y.docs)==null?void 0:S.source}}};var W,j,b;o.parameters={...o.parameters,docs:{...(W=o.parameters)==null?void 0:W.docs,source:{originalSource:"{}",...(b=(j=o.parameters)==null?void 0:j.docs)==null?void 0:b.source}}};var f,T,L;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:"{}",...(L=(T=n.parameters)==null?void 0:T.docs)==null?void 0:L.source}}};const E=["Overview","WithVariantHighlightLight","WithVariantHighlightDark","WithVariantSideImage","WithContentTypeTutorial","WithIsLoading"];export{r as Overview,o as WithContentTypeTutorial,n as WithIsLoading,e as WithVariantHighlightDark,s as WithVariantHighlightLight,i as WithVariantSideImage,E as __namedExportsOrder,O as default};
